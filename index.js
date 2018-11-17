8614b2ab60b436cb0fbec88bf9ca34720929f4a4
Fei Qi <feiqi9047@gmail.com>
2018-11-17 18:16:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function destructivelyRemoveFirstKitten(name){
   return kittens 
 }
 
-function appendKitten(kittens){
+function appendKitten(kittens, name){
   kittens = [...kittens, "Broom"]
   return kittens
 }
